6ac8db663040d448c0fe891822a3ab9bfdbffe3d
wallgate <kilarbox@yandex.ru>
2010-10-19 21:36:03 +0400

    интеграция с Doctrine

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -3,6 +3,34 @@
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
 
+    public function _initDoctrine()
+    {
+        $this->getApplication()
+             ->getAutoloader()
+             ->pushAutoloader(array('Doctrine', 'autoload'))
+             ->pushAutoloader(array('Doctrine', 'modelsAutoload'));
 
+        $dbConfig       = $this->getOption('db');
+        $doctrineConfig = $this->getOption('doctrine');
+
+        $manager = Doctrine_Manager::getInstance();
+        $manager->setAttribute(Doctrine::ATTR_MODEL_LOADING, $doctrineConfig['models_loading']);
+        $manager->setAttribute(Doctrine::ATTR_EXPORT, Doctrine::EXPORT_ALL);
+        $manager->setAttribute(Doctrine::ATTR_VALIDATE, Doctrine::VALIDATE_ALL);
+        $manager->setAttribute(Doctrine::ATTR_QUOTE_IDENTIFIER, true);
+
+        $dsn = sprintf("mysql://%s:%s@%s/%s",
+            $dbConfig['username'],
+            $dbConfig['password'],
+            $dbConfig['host'],
+            $dbConfig['dbname']
+        );
+
+        $connection = $manager->connection($dsn);
+
+        Doctrine::loadModels($doctrineConfig['models_path']);
+
+        return $connection;
+    }
 }
 
---
+++ public/cli.php
@@ -0,0 +1,22 @@
+<?php
+
+defined('APPLICATION_PATH')
+    || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
+
+define('APPLICATION_ENV', 'cli');
+
+set_include_path(implode(PATH_SEPARATOR, array(
+    realpath(APPLICATION_PATH . '/../library'),
+    realpath(APPLICATION_PATH . '/models'),
+    get_include_path(),
+)));
+
+require_once 'Zend/Application.php';
+
+$application = new Zend_Application( APPLICATION_ENV, APPLICATION_PATH.'/configs/application.ini');
+$application->getBootstrap()->bootstrap('doctrine');
+
+$config = $application->getOption('doctrine');
+
+$cli = new Doctrine_Cli($config);
+$cli->run($_SERVER['argv']);
