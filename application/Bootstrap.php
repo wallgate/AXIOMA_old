e6a5af6658ff477c045d87237faccf71190b2a05
wallgate <kilarbox@yandex.ru>
2010-10-19 22:20:17 +0400

    добавлен layout и первая итерация CSS

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,7 +2,23 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
+    public function _initView()
+    {
+        $this->bootstrap('layout');
+        $layout = $this->getResource('layout');
 
+        $view = $layout->getView();
+
+        $view->doctype('XHTML1_STRICT');
+        $view->headMeta()->appendHttpEquiv('Content-Type', 'text/html;charset=utf-8');
+        $view->headlink()->appendStylesheet('/public/css/main.css');
+
+        $view->addHelperPath('App/View/Helper', 'App_View_Helper');
+
+        return $view;
+    }
+
+    
     public function _initDoctrine()
     {
         $this->getApplication()
