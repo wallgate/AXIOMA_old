294c9f3e5857b4cb19772f4749867bbf1fe9585b
wallgate <kilarbox@yandex.ru>
2010-10-22 16:20:33 +0400

    декоратор, объединяющий в себе декораторы Label и Description

---
+++ library/App/Form/Decorator/InfoCell.php
@@ -0,0 +1,25 @@
+<?php
+
+class App_Form_Decorator_InfoCell extends Zend_Form_Decorator_Abstract
+{
+    public function render($content)
+    {
+        $label       = $this->getElement()->getLabel();
+        $description = $this->getElement()->getDescription();
+        $infoCell    = $label . '<br/><small class="description">' . $description . '</small>';
+        
+        $placement   = $this->getPlacement();
+        $htmlTag     = $this->getOption('tag');
+        $tagClass    = $this->getOption('wrapperClass');
+
+        if (!empty($htmlTag))
+        {
+            if (!empty($tagClass)) $class = ' class="'.$tagClass.'"';
+            $infoCell = sprintf('<%s%s>%s</%s>', $htmlTag, $class, $infoCell, $htmlTag);
+        }
+
+        return $placement=='append'
+            ? $content . $infoCell
+            : $infoCell . $content;
+    }
+}
