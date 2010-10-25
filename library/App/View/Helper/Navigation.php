91a846ce7eb86433f712465c5d8eb3f8b29d7ef5
wallgate <kilarbox@yandex.ru>
2010-10-25 12:47:11 +0400

    в хелпер Navigation добавлена заглушка для метода, отображающего заголовок страницы

--- library/App/View/Helper/Navigation.php
+++ library/App/View/Helper/Navigation.php
@@ -2,8 +2,16 @@
 
 class App_View_Helper_Navigation extends Zend_View_Helper_Navigation
 {
+    protected $config;
+
     public function navigation($config)
     {
+        $this->config = $config;
         return parent::navigation(new App_Navigation($config));
+    }
+
+    public function headline()
+    {
+        return false;
     }
 }
